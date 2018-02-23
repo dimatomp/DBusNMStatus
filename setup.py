8d456aceab807265321dbf6eec322baac71afb56
Dmitry Tomp <dmitrytomp@gmail.com>
2018-02-24 00:21:13 +0300

    Try removing dbus from requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     description="A script that prints current Wi-Fi connection info in readable format",
     long_description="A script that subscribes to NetworkManager via DBus and prints SSID and signal strength of current Wi-Fi connection in readable format",
-    install_requires=["dbus-python>=1.2.4", "PyGObject>=3.24.1"],
+    install_requires="PyGObject>=3.24.1",
     url="http://github.com/dimatomp/DBusNMStatus",
     author="Dmitry Tomp",
     author_email="dmitrytomp@gmail.com",
