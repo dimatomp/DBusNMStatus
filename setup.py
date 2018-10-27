9ec591f284feb0f58b8650700eec01411198700e
Dmitry Tomp <dmitrytomp@gmail.com>
2018-10-27 12:06:17 +0300

    Reduce PyGObject requirement version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     description="A script that prints current Wi-Fi connection info in readable format",
     long_description="A script that subscribes to NetworkManager via DBus and prints SSID and signal strength of current Wi-Fi connection in readable format",
-    install_requires="PyGObject>=3.24.1",
+    install_requires="PyGObject>=3.18.2",
     url="http://github.com/dimatomp/DBusNMStatus",
     author="Dmitry Tomp",
     author_email="dmitrytomp@gmail.com",
